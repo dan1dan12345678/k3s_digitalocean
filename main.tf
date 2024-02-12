3ae2101800851a158ea8238c2d6328340ccf947f
dan1dan12345678 <47665212+dan1dan12345678@users.noreply.github.com>
2024-02-12 11:25:55 +0100

    added argocd deployment

--- main.tf
+++ main.tf
@@ -23,3 +23,38 @@ resource "digitalocean_kubernetes_cluster" "terraformk3s" {
     max_nodes = 4
   }
 }
+
+# Argocd deploy wia Helm chart
+# fetch kubeconfig
+output "kubeconfig" {
+  value = digitalocean_kubernetes_cluster.terraformk3s.kube_config[0].raw_config
+}
+
+# Set up the Kubernetes provider using the kubeconfig from the DigitalOcean cluster.
+
+provider "kubernetes" {
+  config_path = digitalocean_kubernetes_cluster.terraformk3s.kube_config[0].raw_config
+}
+
+# Helm provider to deploy ArgoCD using a Helm chart.
+provider "helm" {
+  kubernetes {
+    config_path = digitalocean_kubernetes_cluster.terraformk3s.kube_config[0].raw_config
+  }
+}
+
+# Deploying ArgoCD
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "1.6.1"  # Specify the chart version
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  # You can add more `set` blocks to customize the ArgoCD deployment
+}
